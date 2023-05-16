1ba18c3b96baa04b432cc9cf140efe0420567c82
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-05-16 13:47:19 +0200

    model without attention

--- image_caption/model.py
+++ image_caption/model.py
@@ -69,6 +69,72 @@ class DecoderRNN(nn.Module):
 
 
 class CNNtoRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
+        super(CNNtoRNN, self).__init__()
+
+        # Instantiate the EncoderCNN module
+        self.EncoderCNN = EncoderCNN(embed_size)
+
+        # Instantiate the DecoderRNN module
+        self.DecoderRNN = DecoderRNN(embed_size, hidden_size, vocab_size, num_layers)
+
+    def forward(self, images, captions):
+        # Pass the images through the EncoderCNN module to extract features
+        features = self.EncoderCNN(images)
+
+        # Pass the features and captions through the DecoderRNN module
+        outputs = self.DecoderRNN(features, captions)
+
+        # Return the final output logits
+        return outputs
     
-    dsgsdfgfgsdgdg
-    
+    def caption_image(self, image, vocabulary, max_length = 40):
+        # Initialize an empty list to store the generated caption
+        result_caption = []
+        
+        # Disable gradient calculation
+        with torch.no_grad():
+            
+            # Add a batch dimension to the image tensor
+            x = self.EncoderCNN.unsqueeze(0)
+            
+            # Initialize the LSTM states to None
+            states = None
+             
+            # Generate the caption word by word 
+            for _ in range(max_length):
+                
+                # Pass the image features and LSTM states through the LSTM layer
+                hiddens, states = self.DecoderRNN.lstm(x, states)
+                
+                # Pass the LSTM output through the linear layer to get the output logits
+                output = DecoderRNN.linear(hiddens.unsqueeze(0))
+                
+                # Get the predicted word by taking the index of the highest logit value
+                predicted = output.argmax(1)
+                
+                '''
+                ANOTHER OPTION WOULD BE TO GET THE PREDICTED WORD SAMPLING INSTEAD OF THE PREVIOUS GREEDY APPROACH
+                (neil: a mi me gusta más, decid vosotros)
+                
+                # Apply softmax to convert output logits to probabilities
+        		probs = output.squeeze(0).softmax(dim=0)
+
+        		# Sample a word index from the output probabilities
+        		predicted = Categorical(probs).sample().item()
+                '''
+                
+                # Append the predicted word index to the result caption list
+                result_caption.append(predicted.item())
+                
+                # Embed the predicted word and add a batch dimension
+                x = self.DecoderRNN.embed(predicted).unsqueeze(0)
+                
+                # Check if the predicted word is the end-of-sequence token ("<EOS>")
+                if vocabulary.itos[predicted.item()] == "<EOS>":
+                    break
+        
+        # Convert the list of word indices to a list of actual words using the vocabulary
+        return [vocabulary.itos[idx] for idx in result_caption]
+
+
