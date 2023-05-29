1bd5f3e4023a6936e80fc5bb5b4b0cdc63021567
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-05-30 00:49:11 +0200

    utils.py commented

---
+++ Pixtales/Image_captioning_with_attention/utils.py
@@ -0,0 +1,17 @@
+import torch  
+import torch.nn.functional as F
+import numpy as np
+
+# Save model state to file
+def save_checkpoint(state, filename="checkpoint.pth"):
+    torch.save(state, filename)  # Save model state
+    print("Checkpoint saved!")
+
+# Load model state from file
+def load_checkpoint(checkpoint, model, optimizer):
+    model.load_state_dict(checkpoint['state_dict'])  # Load model parameters
+    optimizer.load_state_dict(checkpoint['optimizer'])  # Load optimizer state
+    scheduler.load_state_dict(checkpoint['scheduler'])  # Load scheduler state
+    step = checkpoint['step']  # Retrieve step number
+    print("Checkpoint loaded!")
+    return step
