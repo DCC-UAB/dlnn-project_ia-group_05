102194dba29d050b416d9ad5ea336b79d3ffb2e5
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2023-05-30 09:07:36 +0200

    Update utils.py

--- Pixtales/Image_captioning_with_attention/utils.py
+++ Pixtales/Image_captioning_with_attention/utils.py
@@ -8,10 +8,11 @@ def save_checkpoint(state, filename="checkpoint.pth"):
     print("Checkpoint saved!")
 
 # Load model state from file
-def load_checkpoint(checkpoint, model, optimizer):
+def load_checkpoint(checkpoint, model, optimizer, scheduler):
     model.load_state_dict(checkpoint['state_dict'])  # Load model parameters
     optimizer.load_state_dict(checkpoint['optimizer'])  # Load optimizer state
     scheduler.load_state_dict(checkpoint['scheduler'])  # Load scheduler state
     step = checkpoint['step']  # Retrieve step number
     print("Checkpoint loaded!")
     return step
+
